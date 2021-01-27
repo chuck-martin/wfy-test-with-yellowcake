375d296b3aa8116f254515af459ca12de487ed28
Chuck <chuck_martin@me.com>
2021-01-26 18:52:06 -0800

    Update Footer.js
    Updated with my own info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="">@localtest</a>
+      Follow me{"https://twitter.com/chuck_martin"}
+      <a href="">@chuck_martin</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+          © Copyright {new Date().getFullYear()} All rights reserved. Theme crafted by{' '}
           <a href="https://thriveweb.com.au/">Thrive</a>.
         </span>
       </div>
